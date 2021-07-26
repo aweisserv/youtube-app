dc197d5cad9756d6ed360c665f6580672b82a047
Allan Weisser Verdessi <vredes@live.com>
2021-07-25 21:33:25 -0400

    map pipe working

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,21 +1,24 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { YoutubeResponse } from '../models/youtube.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  private youtubeUrl    = 'https://youtube.googleapis.com/youtube/v3/';
+  private youtubeUrl    = 'https://youtube.googleapis.com/youtube/v3';
   private apikey        = 'AIzaSyCAv_9hbcQjkQgVeaJY-Tp5-qJo7Fncf9I';
   private playlist      = 'UUwNnutxeIqT2n39lcyLGU7Q';
   private nextPageToken = '';
 
   constructor( private http: HttpClient ) { }
 
-  getVideos(){
+  getVideos() {
 
-    const url = `${ this.youtubeUrl }playlistItems`;
+    const url = `${ this.youtubeUrl }/playlistItems`;
 
     const params = new HttpParams()
     .set('part', 'snippet')
@@ -23,8 +26,19 @@ export class YoutubeService {
     .set('playlistId', this.playlist)
     .set('key', this.apikey)
 
-    return this.http.get( url, { params });
+    return this.http.get<YoutubeResponse>( url, { params })
+              .pipe(
 
+                map( resp => {
+                  this.nextPageToken = resp.nextPageToken;
+                  return resp.items;
+                }),
+
+                map( items => {
+                  return items.map( video => video.snippet )
+                })
+
+              )
   }
 
 }
