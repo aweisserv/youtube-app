fb0c9fdcac0af90e4d5c1640ec4e70a4d1686117
Allan Weisser Verdessi <vredes@live.com>
2021-07-30 00:56:07 -0400

    video grid page working

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from 'src/app/services/youtube.service';
 
+import { Video } from 'src/app/models/youtube.models';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,12 +11,19 @@ import { YoutubeService } from 'src/app/services/youtube.service';
 })
 export class HomeComponent implements OnInit {
 
+
+  videos: Video[] = [];
+
   constructor( private youtubeService: YoutubeService ) { }
 
   ngOnInit(): void {
 
     this.youtubeService.getVideos()
-        .subscribe( resp => console.log( resp ));
+        .subscribe( resp => {
+          console.log( resp );
+          this.videos.push( ...resp ); //ECMA SCRIPT 6 PUSH + OPERADOR SPREAD. SUMA NUEVOS RESULTADOS AL ARREGLO DE VIDEOS.
+    });
+
   }
 
 }
